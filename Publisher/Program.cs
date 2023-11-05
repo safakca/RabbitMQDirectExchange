fbb31720372f9d5e7597c7e0210b5ce502f7333f
Şafak AKÇA <40564836+safakca@users.noreply.github.com>
2023-11-05 13:41:25 +0300

    Update Program.cs

--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -2,7 +2,7 @@
 using System.Text;
 
 ConnectionFactory factory = new();
-factory.Uri = new Uri("amqps://geilrjnf:CFHBVh0ApbH-4IfPGkSEB7-hUFVg1wQK@sparrow.rmq.cloudamqp.com/geilrjnf");
+factory.Uri = new Uri("..");
 
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
