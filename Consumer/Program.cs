71ae0511637c862e98110270133e1cb0c3eeab9c
Şafak AKÇA <40564836+safakca@users.noreply.github.com>
2023-11-05 13:41:36 +0300

    Update Program.cs

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -3,7 +3,7 @@ using RabbitMQ.Client.Events;
 using System.Text;
 
 ConnectionFactory factory = new();
-factory.Uri = new Uri("amqps://geilrjnf:CFHBVh0ApbH-4IfPGkSEB7-hUFVg1wQK@sparrow.rmq.cloudamqp.com/geilrjnf");
+factory.Uri = new Uri("..");
 
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
